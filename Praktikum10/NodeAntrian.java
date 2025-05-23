631977b15fd99a7fcfa2beff49fc6a58f06fae31
akhmadhk <akhmadk654@gmail.com>
2025-05-24 05:05:44 +0700

    .

--- Praktikum10/NodeAntrian.java
+++ Praktikum10/NodeAntrian.java
@@ -1,5 +1,11 @@
 package Praktikum10;
 
 public class NodeAntrian {
-    
+    Mahasiswa3 data;
+    NodeAntrian next;  
+
+    public NodeAntrian(Mahasiswa3 mhs) {
+        this.data = mhs;
+        this.next = null;
+    }
 }
--- Praktikum10/QueueAntrian.java
+++ Praktikum10/QueueAntrian.java
@@ -1,5 +1,70 @@
 package Praktikum10;
 
 public class QueueAntrian {
-    
+    NodeAntrian front, rear;
+    int size;
+
+    public QueueAntrian() {
+        front = rear = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return front == null;
+    }
+
+    public void enqueue(Mahasiswa3 data) {
+        NodeAntrian newNode = new NodeAntrian(data);
+        if (isEmpty()) {
+            front = rear = newNode;
+        } else {
+            rear.next = newNode;
+            rear = newNode;
+        }
+        size++;
+    }
+
+    public Mahasiswa3 dequeue() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong");
+            return null;
+        } else {
+            Mahasiswa3 result = front.data;
+            front = front.next;
+            if (front == null) rear = null;
+            size--;
+            return result;
+        }
+    }
+
+    public void printQueue() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong");
+        } else {
+            NodeAntrian temp = front;
+            System.out.println("Daftar Antrian:");
+            while (temp != null) {
+                temp.data.tampilInformasi();
+                temp = temp.next;
+            }
+        }
+    }
+
+    public Mahasiswa3 peekFront() {
+        return isEmpty() ? null : front.data;
+    }
+
+    public Mahasiswa3 peekRear() {
+        return isEmpty() ? null : rear.data;
+    }
+
+    public void clear() {
+        front = rear = null;
+        size = 0;
+        System.out.println("Antrian dikosongkan");
+    }
+
+    public int getSize() {
+        return size;
+    }
 }
